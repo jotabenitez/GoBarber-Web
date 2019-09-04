17e49da58dd989259e82079f3d33ac200fc14721
jota <jota@criptext.com>
2019-09-04 09:37:26 -0300

    working till atualizando perfil

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Form, Input } from '@rocketseat/unform';
 
-// import { Container } from './styles';
+import { updateProfileRequest } from '~/store/modules/user/actions';
+
+import { Container } from './styles';
 
 export default function Profile() {
-  return <h1>Profile</h1>;
+  const dispatch = useDispatch();
+  const profile = useSelector(state => state.user.profile);
+
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
+  return (
+    <Container>
+      <Form initialData={profile} onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome" />
+        <Input name="email" type="email" placeholder="Email" />
+
+        <hr />
+
+        <Input name="oldPassword" type="password" placeholder="Senha" />
+        <Input name="newPassword" type="password" placeholder="Nova Senha" />
+        <Input
+          name="confirmPassword"
+          type="password"
+          placeholder="Confirme sua nova Senha"
+        />
+
+        <button type="submit">Atualizar Perfil</button>
+      </Form>
+      <button type="button">Sair do GoBarber</button>
+    </Container>
+  );
 }
